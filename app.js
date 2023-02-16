4ffa56b6d4c40681eea47f0bd361af99ca8d8bd8
Anton <gadeudin@gmail.com>
2023-02-16 16:57:19 +0300

    добавил роут в app и клиентский fetch

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const config = require('./config/serverConfig');
 const homeRouter = require('./routes/render/home.routes');
 const authRouter = require('./routes/render/auth.routes');
 const commentRouter = require('./routes/render/comment.routes');
+const addBookRouter = require('./routes/render/book.routes');
+
 
 const app = express();
 
@@ -15,6 +17,8 @@ config(app);
 app.use('/', homeRouter);
 app.use('/auth', authRouter);
 app.use('/comment', commentRouter);
+app.use('./book', addBookRouter);
+
 
 app.listen(PORT, () => {
   console.log(`Server started at ${PORT} port`);
--- public/js/application.js
+++ public/js/application.js
@@ -8,6 +8,9 @@ const input = document.getElementById('input');
 // const logOut = document.getElementById('logOut');
 // const regBtn = document.getElementById('regBtn');
 // const logBtn = document.getElementById('logBtn');
+const body = document.querySelector('body');
+const formAddBook = document.querySelector('#addBookForm');
+
 
 if (formReg) {
   formReg.addEventListener('submit', async (event) => {
@@ -53,3 +56,16 @@ if (formLog) {
   });
 }
 
+if (formAddBook) {
+  formAddBook.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const { action, method, nameBook, autor, comment, img } = event.target;
+    const response = await fetch(action, {
+      method: method.value,
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ nameBook: nameBook.value, autor: autor.value, comment: comment.value, img: img.value });
+    })
+    const { newBookHtml, newCommentHtml } = await response.JSON();
+    body.append(newBookHtml) /*newCommentHtml - форма нового коммента возможно не нужна*/
+  })
+}
